95db53276d3da3da22abde94f1de7d476a6969ba
Troydon Luicien <troyincarnate@gmail.com>
2020-01-19 23:34:41 +1100

    sanbox name change

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,16 +8,27 @@ public class Mover : MonoBehaviour
 {
 
     [SerializeField] Transform target;
-
+    Ray lastRay;
     void Start()
     {
-        // isAI = false;
     }
     void Update()
     {
-        // if it is an AI we move to the destination
-            // change the destination for the navmesh agent to the target position. (e.g use player to go to target)
-            GetComponent<NavMeshAgent>().destination = target.position;
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor(); 
+        }
+        // draw the ray
+
+        // change the destination for the navmesh agent to the target position. (e.g use player to go to target)
+
+    }
+
+    public void MoveToCursor()
+    {
+        Physics.Raycast()
+        lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        GetComponent<NavMeshAgent>().destination = target.position;
+
     }
 }
