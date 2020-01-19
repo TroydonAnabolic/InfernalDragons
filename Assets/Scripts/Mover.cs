bc5d54fb73d0d2d5da1f2f300be6030c65e452d3
Troydon Luicien <troyincarnate@gmail.com>
2020-01-19 18:02:07 +1100

    initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    void Update()
+    {
+        // change the destination for the navmesh agent to the target position. (e.g use player to go to target)
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
