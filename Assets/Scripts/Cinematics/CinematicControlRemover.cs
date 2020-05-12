21133df7cd92fd44421699f0e13aee0599b9559f
Troydon Luicien <troyincarnate@gmail.com>
2020-05-13 00:15:38 +1000

    Disabled movement during cinematics

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RPG.Control;
+using RPG.Core;
+using System;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -6,21 +8,23 @@ namespace RPG.Cinematics
 {
     class CinematicControlRemover : MonoBehaviour
     {
+        GameObject player;
         void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
-
         }
 
         private void EnableControl(PlayableDirector obj)
         {
-            print("Enabled Control");
+            player = GameObject.FindWithTag("Player");
+            player.GetComponent<PlayerController>().enabled = true; // player.GetComponent is what we use to grab components on another object
         }
-
         private void DisableControl(PlayableDirector obj)
         {
-            print("Disabled Control");
+            player = GameObject.FindWithTag("Player");
+            player.GetComponent<ActionScheduler>().CancelAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
     }
 }
